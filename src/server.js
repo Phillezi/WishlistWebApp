63fac84aa4dcc2179d55b5b07d74cb0291c285b2
Philip Zingmark <philip.zingmark@gmail.com>
2023-11-20 23:07:04 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -120,6 +120,9 @@ const serveWishlistHtml = (res, base64data) => {
             <div id="token" style="display: none;">${base64data}</div>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="apple-touch-icon" sizes="180x180" href="../icons/apple-touch-icon.png">
+            <link rel="icon" type="image/png" sizes="32x32" href="../icons/favicon-32x32.png">
+            <link rel="icon" type="image/png" sizes="16x16" href="../icons/favicon-16x16.png">
             <title>Wishlist</title>
             <link rel="stylesheet" href="../css/styles.css">
         </head>
